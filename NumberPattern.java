e67e97f76a7bfa932fd153914bb224ee9fb0de80
Suhas-Kopparapu <kopparapu.suhas@gmail.com>
2016-08-16 19:39:25 +0530

    Session2 Assignment 3
    Number pattern

---
+++ NumberPattern.java
@@ -0,0 +1,30 @@
+package Assignments;
+
+public class NumberPattern {
+	
+	public static void main(String[] args){
+		
+		for(int m=1;m<=4;m++){
+			System.out.println("");
+			for(int n=1;n<=m;n++){
+				System.out.print(n);
+			}
+			
+		}
+		
+
+		for(int i=1,r=5;i<=5;i++,r--)
+		{
+			System.out.println("");
+			for(int j=1;j<=r;j++)
+			{
+				System.out.print(j);
+			}
+			
+		}
+		
+		
+	}
+
+}
+ 
